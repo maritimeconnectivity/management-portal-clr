32d9d0e0cb3749b53dda8d3a2d242c51f7ae058f
Jinki <your.jinki.jung@gmail.com>
2024-09-11 21:30:04 +0200

    applying theme

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'mp-header',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   role: string = 'admin';
+
+  constructor(private authService: AuthService) {}
+
+  async logOut() {
+    this.authService.logout();
+  }
 }
