bcd22fc636770f08c93fbc5f1dd4e3a8ce7217f2
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2024-09-17 15:17:34 +0200

    Fix out of place commas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,54 +15,55 @@
  */
 
 declare const fetch: Function
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
 
 export class AppConfig {
-  static OIDC_BASE_PATH: string;
-  static ENDORSEMENT_BASE_PATH: string;
-  static IR_BASE_PATH: string;
-  static HAS_SERVICE_REGISTRY: boolean;
-  static SR_BASE_PATH: string;
-  static ENVIRONMENT_TITLE: string;
-  static IDP_NAMESPACE: string;
-  static ENVIRONMENT_NAME: string;
-  static IR_PROVIDER: string;
-  static IR_CONTACT: string;
-  static SR_PROVIDER: string;
-  static SR_CONTACT: string;
-  static HAS_MSR_LEDGER: boolean;
-  static LEDGER_PATH: string;
-  static MP_PROVIDER: string;
-  static TERMS_OF_USE: string;
-  static MP_CONTACT: string;
-  static MP_VERSION: string;
-  static MP_NAME: string;
-  static FOOTER_NAME: string;
-  static FOOTER_LINK: string;
-  static LOGO_IMG: string;
+    static OIDC_BASE_PATH: string;
+    static ENDORSEMENT_BASE_PATH: string;
+    static IR_BASE_PATH: string;
+    static HAS_SERVICE_REGISTRY: boolean;
+    static SR_BASE_PATH: string;
+    static ENVIRONMENT_TITLE: string;
+    static IDP_NAMESPACE: string;
+    static ENVIRONMENT_NAME: string;
+    static IR_PROVIDER: string;
+    static IR_CONTACT: string;
+    static SR_PROVIDER: string;
+    static SR_CONTACT: string;
+    static HAS_MSR_LEDGER: boolean;
+    static LEDGER_PATH: string;
+    static MP_PROVIDER: string;
+    static TERMS_OF_USE: string;
+    static MP_CONTACT: string;
+    static MP_VERSION: string;
+    static MP_NAME: string;
+    static FOOTER_NAME: string;
+    static FOOTER_LINK: string;
+    static LOGO_IMG: string;
 
-  public static _initialize() {
-    AppConfig.IR_BASE_PATH = environment.irBasePath.replace(/\/$/, ''),
-    AppConfig.SR_BASE_PATH = environment.hasServiceRegistry ? environment.srBasePath.replace(/\/$/, '') : '',
-    AppConfig.ENVIRONMENT_TITLE = environment.environmentTitle,
-    AppConfig.IDP_NAMESPACE = environment.idpNamespace;
-    AppConfig.HAS_SERVICE_REGISTRY = environment.hasServiceRegistry;
-    AppConfig.OIDC_BASE_PATH = environment.oidcBasePath.replace(/\/$/, '');
-    AppConfig.ENVIRONMENT_NAME = environment.environmentName;
-    AppConfig.IR_PROVIDER = environment.irProvider;
-    AppConfig.IR_CONTACT = environment.irContact;
-    AppConfig.SR_PROVIDER = environment.srProvider;
-    AppConfig.SR_CONTACT = environment.srContact;
-    AppConfig.HAS_MSR_LEDGER = environment.hasMSRLedger;
-    AppConfig.LEDGER_PATH = environment.hasMSRLedger ? environment.ledgerPath : '';
-    AppConfig.MP_PROVIDER = environment.mpProvider;
-    AppConfig.MP_NAME = environment.mpName;
-    AppConfig.TERMS_OF_USE = environment.termsOfUse;
-    AppConfig.MP_CONTACT = environment.mpContact;
-    AppConfig.MP_VERSION = "test";
-    AppConfig.FOOTER_NAME = environment.footerName;
-    AppConfig.FOOTER_LINK = environment.footerLink;
-    AppConfig.LOGO_IMG = environment.logoImg;
-  }
+    public static _initialize() {
+        AppConfig.IR_BASE_PATH = environment.irBasePath.replace(/\/$/, '');
+        AppConfig.SR_BASE_PATH = environment.hasServiceRegistry ? environment.srBasePath.replace(/\/$/, '') : '';
+        AppConfig.ENVIRONMENT_TITLE = environment.environmentTitle;
+        AppConfig.IDP_NAMESPACE = environment.idpNamespace;
+        AppConfig.HAS_SERVICE_REGISTRY = environment.hasServiceRegistry;
+        AppConfig.OIDC_BASE_PATH = environment.oidcBasePath.replace(/\/$/, '');
+        AppConfig.ENVIRONMENT_NAME = environment.environmentName;
+        AppConfig.IR_PROVIDER = environment.irProvider;
+        AppConfig.IR_CONTACT = environment.irContact;
+        AppConfig.SR_PROVIDER = environment.srProvider;
+        AppConfig.SR_CONTACT = environment.srContact;
+        AppConfig.HAS_MSR_LEDGER = environment.hasMSRLedger;
+        AppConfig.LEDGER_PATH = environment.hasMSRLedger ? environment.ledgerPath : '';
+        AppConfig.MP_PROVIDER = environment.mpProvider;
+        AppConfig.MP_NAME = environment.mpName;
+        AppConfig.TERMS_OF_USE = environment.termsOfUse;
+        AppConfig.MP_CONTACT = environment.mpContact;
+        AppConfig.MP_VERSION = "test";
+        AppConfig.FOOTER_NAME = environment.footerName;
+        AppConfig.FOOTER_LINK = environment.footerLink;
+        AppConfig.LOGO_IMG = environment.logoImg;
+    }
 }
+
 AppConfig._initialize();
