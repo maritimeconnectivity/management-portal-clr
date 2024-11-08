8287e2826ecb10ac34a9cd6110d796f20c079a51
Jinki <your.jinki.jung@gmail.com>
2024-11-08 19:42:53 +0100

    list formatting

--- src/app/common/columnForMenu.ts
+++ src/app/common/columnForMenu.ts
@@ -977,7 +977,7 @@ export const ColumnForResource: {[key: string]: object} = {
     },
     unlocode: {
       title: 'Unlocode',
-      type: 'stringArray',
+      type: 'string',
       placeholder: 'Please enter UN/LOCODE',
       visibleFrom: ['detail', 'edit', 'edit-new'],
     },
@@ -1114,7 +1114,7 @@ export const ColumnForResource: {[key: string]: object} = {
     },
     orgCountry: {
       title: 'Country of organization',
-      type: 'string',
+      type: 'stringArray',
       description: 'Country that organization belongs to',
       visibleFrom: ['detail', 'edit', 'edit-new'],
       options: countryOptions,
