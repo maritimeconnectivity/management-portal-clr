b9b422b53ed491ccb89552e9430bcb30e00b10b7
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2025-09-12 09:29:22 +0200

    Replace `:` in certificate bundle file names

--- src/app/common/shared/file-helper.service.ts
+++ src/app/common/shared/file-helper.service.ts
@@ -16,164 +16,164 @@
 
 import {Injectable} from '@angular/core';
 
-import * as fileSaver from 'file-saver';
-import * as JSZip from 'jszip';
-import { CertificateBundle } from '../certificateBundle';
-import { DocControllerService, DocDto, XmlControllerService, XmlDto } from 'src/app/backend-api/service-registry';
-import { NotifierService } from 'gramli-angular-notifier';
-import { TranslateService } from '@ngx-translate/core';
+import fileSaver from 'file-saver';
+import JSZip from 'jszip';
+import {CertificateBundle} from '../certificateBundle';
+import {DocControllerService, DocDto, XmlControllerService, XmlDto} from 'src/app/backend-api/service-registry';
+import {NotifierService} from 'gramli-angular-notifier';
+import {TranslateService} from '@ngx-translate/core';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class FileHelperService {
-  constructor(private translate: TranslateService,
-    private notifierService: NotifierService,
-    private xmlControllerService: XmlControllerService,
-    private docControllerService: DocControllerService,
-  ) {
+    constructor(private translate: TranslateService,
+                private notifierService: NotifierService,
+                private xmlControllerService: XmlControllerService,
+                private docControllerService: DocControllerService,
+    ) {
 
-  }
-
-  public downloadPemCertificate(certificateBundle: CertificateBundle, entityName: string) {
-    try {
-      const nameNoSpaces = entityName.split(' ').join('_');
-
-      const zip = new JSZip();
-      zip.file("Certificate_" + nameNoSpaces + ".pem", certificateBundle.certificate!);
-      if (certificateBundle.privateKey) {
-        zip.file("PrivateKey_" + nameNoSpaces + ".pem", certificateBundle.privateKey);
-      }
-      if (certificateBundle.publicKey) {
-        zip.file("PublicKey_" + nameNoSpaces + ".pem", certificateBundle.publicKey);
-      }
-      if (certificateBundle.pkcs12Keystore) {
-        zip.file("Keystore_" + nameNoSpaces + ".p12", certificateBundle.pkcs12Keystore);
-      }
-      if (certificateBundle.keystorePassword) {
-        zip.file("KeystorePassword_" + nameNoSpaces + ".txt", certificateBundle.keystorePassword);
-      }
-      zip.generateAsync({type: "blob"}).then(function (content) {
-        fileSaver.saveAs(content, "Certificate_" + nameNoSpaces + ".zip");
-      });
-    } catch (error) {
-      this.notifierService.notify('error', this.translate.instant('error.file.downloadcert') + error);
     }
-  }
 
-  public downloadXml(xmlFile: XmlDto): void {
-    if (!xmlFile) {
-      this.notifierService.notify('error', this.translate.instant('error.file.empty'));
-      return;
-    }
-    let fileContent = xmlFile.content;
+    public downloadPemCertificate(certificateBundle: CertificateBundle, entityName: string) {
+        try {
+            const nameNoSpacesOrColons = entityName.replaceAll(/(\s|:)/, '_');
 
-    let fileName = xmlFile.name;
-    let fileType = xmlFile.contentContentType!;
-    this.downloadFile(fileContent, fileType, fileName);
-  }
-
-  public downloadDoc(docFile: DocDto): void {
-    if (!docFile) {
-      this.notifierService.notify('error', this.translate.instant('error.file.empty'));
-      return;
-    }
-    // TODO: I belive it is wrong that "content" is an array of strings. Please be wary of this may change in the future
-    if (docFile.filecontent.length > 1 && docFile.filecontent.length < 10) {
-      this.notifierService.notify('error', this.translate.instant('error.file.wrongformat') + docFile.name);
-      return;
-    }
-    let fileContent = docFile.filecontent.toString();
-
-    let fileName = docFile.name;
-    let fileType = docFile.filecontentContentType!;
-    this.downloadBase64File(fileContent, fileType, fileName);
-  }
-
-  public downloadBase64File(base64Content: string, fileType: string, fileName: string): void {
-    try {
-      let byteArray = this.convertBase64ToByteArray(base64Content);
-
-      let blob = new Blob([byteArray], {type: fileType});
-      fileSaver.saveAs(blob, fileName);
-    } catch (error) {
-      this.notifierService.notify('error', this.translate.instant('error.file.downloaderror') + error);
-    }
-  }
-
-  public downloadFile(content: string, fileType: string, fileName: string): void {
-    try {
-      let blob = new Blob([content], {type: fileType});
-      fileSaver.saveAs(blob, fileName);
-    } catch (error) {
-      this.notifierService.notify('error', this.translate.instant('error.file.downloaderror') + error);
-    }
-  }
-
-  public deleteDoc(docId: number): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      this.docControllerService.deleteDoc(docId!).subscribe(
-        () => {
-          this.notifierService.notify('success', this.translate.instant('success.file.delete'));
-          resolve(true);
-        },
-        (error) => {
-          this.notifierService.notify('error', this.translate.instant('error.file.delete') + error);
-          reject(false);
+            const zip = new JSZip();
+            zip.file("Certificate_" + nameNoSpacesOrColons + ".pem", certificateBundle.certificate!);
+            if (certificateBundle.privateKey) {
+                zip.file("PrivateKey_" + nameNoSpacesOrColons + ".pem", certificateBundle.privateKey);
+            }
+            if (certificateBundle.publicKey) {
+                zip.file("PublicKey_" + nameNoSpacesOrColons + ".pem", certificateBundle.publicKey);
+            }
+            if (certificateBundle.pkcs12Keystore) {
+                zip.file("Keystore_" + nameNoSpacesOrColons + ".p12", certificateBundle.pkcs12Keystore);
+            }
+            if (certificateBundle.keystorePassword) {
+                zip.file("KeystorePassword_" + nameNoSpacesOrColons + ".txt", certificateBundle.keystorePassword);
+            }
+            zip.generateAsync({type: "blob"}).then(function (content) {
+                fileSaver.saveAs(content, "Certificate_" + nameNoSpacesOrColons + ".zip");
+            });
+        } catch (error) {
+            this.notifierService.notify('error', this.translate.instant('error.file.downloadcert') + error);
         }
-      );
-    });
-  }
-
-  public deleteXml(xmlFile: XmlDto): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      this.xmlControllerService.deleteXml(xmlFile.id!).subscribe(
-        () => {
-          this.notifierService.notify('success', this.translate.instant('success.file.delete') + xmlFile.name);
-          resolve(true);
-        },
-        (error) => {
-          this.notifierService.notify('error', this.translate.instant('error.file.delete') + xmlFile.name + error);
-          reject(false);
-        }
-      );
-    });
-  }
-
-  public uploadDoc = async (doc: DocDto): Promise<DocDto> => {
-    return new Promise((resolve, reject) => this.docControllerService.createDoc(doc).subscribe(
-      (res) => {
-        this.notifierService.notify('success', this.translate.instant('success.file.upload') + doc.name);
-        resolve(res);
-      },
-      (error) => {
-        this.notifierService.notify('error', this.translate.instant('error.file.upload') + doc.name + error);
-        reject(error);
-      }
-    ));
-  }
-  
-
-  public uploadXml = async (xml: XmlDto): Promise<XmlDto> => {
-    return new Promise((resolve, reject) => this.xmlControllerService.createXml(xml).subscribe(
-      (res) => {
-        this.notifierService.notify('success', this.translate.instant('success.file.upload') + xml.name);
-        resolve(res);
-      },
-      (error) => {
-        this.notifierService.notify('error', this.translate.instant('error.file.upload') + xml.name + error);
-        reject(error);
-      }
-    ));
-  }
-
-  private convertBase64ToByteArray(base64Content: string): Uint8Array {
-    let byteCharacters = window.atob(base64Content);
-    let byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
-    return new Uint8Array(byteNumbers);
-  }
+
+    public downloadXml(xmlFile: XmlDto): void {
+        if (!xmlFile) {
+            this.notifierService.notify('error', this.translate.instant('error.file.empty'));
+            return;
+        }
+        const fileContent = xmlFile.content;
+
+        const fileName = xmlFile.name;
+        const fileType = xmlFile.contentContentType!;
+        this.downloadFile(fileContent, fileType, fileName);
+    }
+
+    public downloadDoc(docFile: DocDto): void {
+        if (!docFile) {
+            this.notifierService.notify('error', this.translate.instant('error.file.empty'));
+            return;
+        }
+        // TODO: I belive it is wrong that "content" is an array of strings. Please be wary of this may change in the future
+        if (docFile.filecontent.length > 1 && docFile.filecontent.length < 10) {
+            this.notifierService.notify('error', this.translate.instant('error.file.wrongformat') + docFile.name);
+            return;
+        }
+        const fileContent = docFile.filecontent.toString();
+
+        const fileName = docFile.name;
+        const fileType = docFile.filecontentContentType!;
+        this.downloadBase64File(fileContent, fileType, fileName);
+    }
+
+    public downloadBase64File(base64Content: string, fileType: string, fileName: string): void {
+        try {
+            const byteArray = this.convertBase64ToByteArray(base64Content);
+
+            const blob = new Blob([byteArray], {type: fileType});
+            fileSaver.saveAs(blob, fileName);
+        } catch (error) {
+            this.notifierService.notify('error', this.translate.instant('error.file.downloaderror') + error);
+        }
+    }
+
+    public downloadFile(content: string, fileType: string, fileName: string): void {
+        try {
+            const blob = new Blob([content], {type: fileType});
+            fileSaver.saveAs(blob, fileName);
+        } catch (error) {
+            this.notifierService.notify('error', this.translate.instant('error.file.downloaderror') + error);
+        }
+    }
+
+    public deleteDoc(docId: number): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.docControllerService.deleteDoc(docId!).subscribe(
+                () => {
+                    this.notifierService.notify('success', this.translate.instant('success.file.delete'));
+                    resolve(true);
+                },
+                (error) => {
+                    this.notifierService.notify('error', this.translate.instant('error.file.delete') + error);
+                    reject(false);
+                }
+            );
+        });
+    }
+
+    public deleteXml(xmlFile: XmlDto): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.xmlControllerService.deleteXml(xmlFile.id!).subscribe(
+                () => {
+                    this.notifierService.notify('success', this.translate.instant('success.file.delete') + xmlFile.name);
+                    resolve(true);
+                },
+                (error) => {
+                    this.notifierService.notify('error', this.translate.instant('error.file.delete') + xmlFile.name + error);
+                    reject(false);
+                }
+            );
+        });
+    }
+
+    public uploadDoc = async (doc: DocDto): Promise<DocDto> => {
+        return new Promise((resolve, reject) => this.docControllerService.createDoc(doc).subscribe(
+            (res) => {
+                this.notifierService.notify('success', this.translate.instant('success.file.upload') + doc.name);
+                resolve(res);
+            },
+            (error) => {
+                this.notifierService.notify('error', this.translate.instant('error.file.upload') + doc.name + error);
+                reject(error);
+            }
+        ));
+    }
+
+
+    public uploadXml = async (xml: XmlDto): Promise<XmlDto> => {
+        return new Promise((resolve, reject) => this.xmlControllerService.createXml(xml).subscribe(
+            (res) => {
+                this.notifierService.notify('success', this.translate.instant('success.file.upload') + xml.name);
+                resolve(res);
+            },
+            (error) => {
+                this.notifierService.notify('error', this.translate.instant('error.file.upload') + xml.name + error);
+                reject(error);
+            }
+        ));
+    }
+
+    private convertBase64ToByteArray(base64Content: string): Uint8Array {
+        const byteCharacters = window.atob(base64Content);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        return new Uint8Array(byteNumbers);
+    }
 
 }
