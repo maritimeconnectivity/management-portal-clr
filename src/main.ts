1d5cd6be101ac18f2b0142c35b48543e7c45ee8d
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2026-03-19 12:49:01 +0100

    Make it possible to load config from a JSON file on runtime Inspired by https://github.com/maritimeconnectivity/MCP-Portal/commit/40bdec08d6317f82b2e2aff29f02880a827ac705

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
+import {AppConfig} from "./app/app.config";
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+AppConfig._initialize().then(() => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+        .catch(err => console.error(err));
+});
