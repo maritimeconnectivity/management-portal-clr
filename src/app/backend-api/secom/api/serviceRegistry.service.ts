7acfc17293e07bd8a774f8342cfe5c03ba2a33a2
jakob22r <jakob.svenningsen@outlook.com>
2026-04-30 11:02:23 +0200

    Remove outcommented code

--- src/app/backend-api/secom/api/serviceRegistry.service.ts
+++ src/app/backend-api/secom/api/serviceRegistry.service.ts
@@ -38,13 +38,13 @@ export class ServiceRegistryService {
     public configuration = new Configuration();
 
     constructor(protected httpClient: HttpClient, @Optional()@Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
-        // if (basePath) {
-        //     this.basePath = basePath;
-        // }
-        // if (configuration) {
-        //     this.configuration = configuration;
-        //     this.basePath = basePath || configuration.basePath || this.basePath;
-        // }
+        if (basePath) {
+            this.basePath = basePath;
+        }
+        if (configuration) {
+            this.configuration = configuration;
+            this.basePath = basePath || configuration.basePath || this.basePath;
+        }
     }
 
     /**
